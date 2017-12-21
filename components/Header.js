8079719a8d8a3e828349351c72f172c30dcafeed
Morozov-FrontEnd <morozov@brigadunado.ru>
2017-12-21 12:50:37 +0300

    Header styled

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,15 @@
+import styled from 'styled-components';
+
+const HeaderLine = styled.header`
+  display: block;
+  background: red;
+`
+
 const Header = () =>{
   return (
-  <header>
-    <img src="../src/img/logo.png"/>
-  </header>);
+  <HeaderLine>
+    <img src="/static/img/logo.png"/>
+  </HeaderLine>);
 }
 
 export default Header
